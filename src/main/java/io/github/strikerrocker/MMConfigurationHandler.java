4626fffb0d0d8765f1667dbee30fb6f006270f04
Guntram Blohm <gbl@guntram.de>
2020-08-02 16:02:24 +0200

    1.16.2-pre1

--- src/main/java/io/github/strikerrocker/MMConfigurationHandler.java
+++ src/main/java/io/github/strikerrocker/MMConfigurationHandler.java
@@ -4,9 +4,6 @@ import de.guntram.mcmod.fabrictools.ConfigurationProvider;
 import de.guntram.mcmod.fabrictools.GuiModOptions;
 import io.github.prospector.modmenu.api.ConfigScreenFactory;
 import io.github.prospector.modmenu.api.ModMenuApi;
-import java.util.Optional;
-import java.util.function.Supplier;
-import net.minecraft.client.gui.screen.Screen;
 
 public class MMConfigurationHandler implements ModMenuApi
 {
@@ -15,11 +12,6 @@ public class MMConfigurationHandler implements ModMenuApi
         return ClearDespawn.MODID;
     }
 
-    @Override
-    public Optional<Supplier<Screen>> getConfigScreen(Screen screen) {
-        return Optional.of(new GuiModOptions(screen, ClearDespawn.MODNAME, ConfigurationProvider.getHandler(ClearDespawn.MODNAME)));
-    }
-    
     @Override
     public ConfigScreenFactory getModConfigScreenFactory() {
         return screen -> new GuiModOptions(screen, ClearDespawn.MODNAME, ConfigurationProvider.getHandler(ClearDespawn.MODNAME));
