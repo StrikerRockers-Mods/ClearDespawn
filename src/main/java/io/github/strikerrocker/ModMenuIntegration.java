814c2587e08036d68d96da1f2e6062f3a5dc6e1d
StrikerRocker <strikerrockerplays@gmail.com>
2021-11-03 18:09:43 +0530

    Remove unwanted stuff and switch to cloth config.

---
+++ src/main/java/io/github/strikerrocker/ModMenuIntegration.java
@@ -0,0 +1,13 @@
+package io.github.strikerrocker;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import me.shedaniel.autoconfig.AutoConfig;
+
+public class ModMenuIntegration implements ModMenuApi {
+
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return screen -> AutoConfig.getConfigScreen(ModConfig.class, screen).get();
+    }
+}
