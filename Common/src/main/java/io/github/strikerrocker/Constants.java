e53107db1a7925437d7eb240439b4ab405b131b6
StrikerRocker <strikerrockerplays@gmail.com>
2022-03-08 23:24:49 +0530

    Add curseforge release from tags and 1.18.2 update.

--- Common/src/main/java/io/github/strikerrocker/Constants.java
+++ Common/src/main/java/io/github/strikerrocker/Constants.java
@@ -1,11 +1,11 @@
 package io.github.strikerrocker;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Constants {
 
     public static final String MOD_ID = "cleardespawn";
     public static final String MOD_NAME = "ClearDespawn";
-    public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
 }
